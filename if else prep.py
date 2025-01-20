98ef0836ad9e2119875971c728450190574fbcf0
wahajraza27 <wahajraza27@gmail.com>
2025-01-20 20:28:27 +0500

    first commit

---
+++ if else prep.py
@@ -0,0 +1,23 @@
+x=str(input("Enter any alphabet:"))
+if x=="a":
+    print("vowel")
+elif x=="e":
+    print("vowel")
+elif x=="i":
+    print("vowel")
+elif x=="o":
+    print("vowel")
+elif x=="u":
+    print("vowel")
+elif x=="A":
+    print("vowel")
+elif x=="E":
+    print("vowel")
+elif x=="I":
+    print("vowel")
+elif x=="O":
+    print("vowel")
+elif x=="U":
+    print("vowel")
+else:
+    print("consonent")
